c127cdfbc81b764b757979fcc2257b8335d4c89a
jeremy avnet <brainsik@users.noreply.github.com>
2016-12-15 09:55:12 -0800

    Handle case where there are local SSH keys

--- stow.sh
+++ stow.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -o pipefail
+set -eo pipefail
 
 # Start with all packages
 PACKAGES=$(ls -1d * | grep -Ev 'stow.sh')
@@ -13,6 +13,11 @@ fi
 # Drop encrypted package if unsupported
 if ! command -v git-crypt >/dev/null; then
     PACKAGES=$(echo $PACKAGES | sed -e 's/_encrypted//')
+fi
+
+# Drop SSH stuff if we're on host with keys already
+if [[ -e ~/.ssh/id_ed25519.pub ]]; then
+    PACKAGES=$(echo $PACKAGES | sed -e 's/ssh//')
 fi
 
 stow $* $PACKAGES
