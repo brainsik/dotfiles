ee1ec46d083752177a6320c5b90916c5926667c9
jeremy avnet <brainsik@users.noreply.github.com>
2016-12-27 10:51:22 -0800

    DEBUG logic (default false)

--- stow.sh
+++ stow.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 set -eo pipefail
+DEBUG () { false; }
 
 # Start with all packages
 PACKAGES=$(ls -1d * | grep -Ev 'stow.sh' | perl -pe 's/\n/ /g;')
-echo "PACKAGES=$PACKAGES"
+DEBUG && echo "PACKAGES=$PACKAGES"
  
 # Drop Darwin only packages
 if [[ $(uname -s) != "Darwin" ]]; then
     PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?homebrew//')
 fi
-echo "PACKAGES=$PACKAGES"
+DEBUG && echo "PACKAGES=$PACKAGES"
 
 # Drop encrypted package if unsupported
 if ! command -v git-crypt >/dev/null; then
     echo "- Skipping encrypted files"
     PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?_encrypted//')
 fi
-echo "PACKAGES=$PACKAGES"
+DEBUG && echo "PACKAGES=$PACKAGES"
 
 # Drop SSH stuff if we're on host with keys already
 if test -f ~/.ssh/id_ed25519.pub && ! test -L ~/.ssh/id_ed25519.pub; then
     echo "- Skipping ssh (keys exist)"
     PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?ssh//')
 fi
-echo "PACKAGES=$PACKAGES"
+DEBUG && echo "PACKAGES=$PACKAGES"
 
 stow $* $PACKAGES
