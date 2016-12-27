5180131ecceb9846d95673da21935043f801677e
jeremy avnet <brainsik@users.noreply.github.com>
2016-12-27 10:39:51 -0800

    Switch to perl

--- stow.sh
+++ stow.sh
@@ -2,23 +2,27 @@
 set -eo pipefail
 
 # Start with all packages
-PACKAGES=$(ls -1d * | grep -Ev 'stow.sh')
+PACKAGES=$(ls -1d * | grep -Ev 'stow.sh' | perl -pe 's/\n/ /g;')
+echo "PACKAGES=$PACKAGES"
  
 # Drop Darwin only packages
 if [[ $(uname -s) != "Darwin" ]]; then
-    PACKAGES=$(echo $PACKAGES | sed -e 's/ ?homebrew//')
+    PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?homebrew//')
 fi
+echo "PACKAGES=$PACKAGES"
 
 # Drop encrypted package if unsupported
 if ! command -v git-crypt >/dev/null; then
     echo "- Skipping encrypted files"
-    PACKAGES=$(echo $PACKAGES | sed -e 's/ ?_encrypted//')
+    PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?_encrypted//')
 fi
+echo "PACKAGES=$PACKAGES"
 
 # Drop SSH stuff if we're on host with keys already
 if test -f ~/.ssh/id_ed25519.pub && ! test -L ~/.ssh/id_ed25519.pub; then
     echo "- Skipping ssh (keys exist)"
-    PACKAGES=$(echo $PACKAGES | sed -e 's/ ?ssh//')
+    PACKAGES=$(echo $PACKAGES | perl -pe 's/ ?ssh//')
 fi
+echo "PACKAGES=$PACKAGES"
 
 stow $* $PACKAGES
