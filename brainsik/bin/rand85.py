778ded574a7bb4619f5cdcc97f519c4c750f6ca7
jeremy avnet <brainsik@users.noreply.github.com>
2017-08-09 20:56:36 -0700

    latest rand85

--- brainsik/bin/rand85.py
+++ brainsik/bin/rand85.py
@@ -9,13 +9,13 @@ def printem(n_bytes):
     msg = "Generating {} bit random ascii85 strings".format(n_bytes * 8)
     print(msg)
     print("-" * len(msg))
-    for _ in range(5):
+    for _ in range(3):
         print(str(a85encode(os.urandom(n_bytes)), 'ascii'))
     return
 
 
 def main(n_bits=None):
-    for bits in ([n_bits] if n_bits else [64, 80, 96, 128, 256]):
+    for bits in ([n_bits] if n_bits else [32, 64, 80, 96, 112, 128, 256]):
         printem(math.ceil(bits / 8))  # round up to bytes
         print()
 
